fe1b7d5b438f535bb37b51561ec7b8c78b33bbde
aosfather <aosfather@gmail.com>
2017-12-05 17:16:54 +0800

    add ocr by baidu

--- httputils.go
+++ httputils.go
@@ -2,8 +2,10 @@ package comb
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
@@ -20,6 +22,30 @@ func Post(theUrl string, data interface{}, result interface{}) error {
 		return err
 	}
 
+	json.Unmarshal(body, result)
+	return nil
+}
+
+func PostByForm(theUrl string, params map[string]string, result interface{}) error {
+
+	var values url.Values = make(map[string][]string)
+	for key, val := range params {
+		values.Set(key, val)
+	}
+	resp, err := http.PostForm(theUrl, values)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	fmt.Println(string(body))
 	json.Unmarshal(body, result)
 	return nil
 }
